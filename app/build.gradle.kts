f99c46d4882ab751643625c2f25955bc81d014c1
amalhanaja <amalhanaja@gmail.com>
2020-03-29 00:29:29 +0700

    Bump Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,16 +60,16 @@ android {
 
     productFlavors {
         create("development") {
-            versionCode = 4
-            versionName = "1.0.4"
+            versionCode = 5
+            versionName = "1.0.5"
             applicationIdSuffix = ".dev"
             versionNameSuffix = "-DEV"
             resValue("string", "app_name", stringProperty("${name}.appName"))
             buildConfigField("String", "BASE_URL", stringProperty("${name}.baseUrl"))
         }
         create("production") {
-            versionCode = 4
-            versionName = "1.0.4"
+            versionCode = 5
+            versionName = "1.0.5"
             resValue("string", "app_name", stringProperty("${name}.appName"))
             buildConfigField("String", "BASE_URL", stringProperty("${name}.baseUrl"))
         }
